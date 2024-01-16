6fb202c9dcb29eba8f2c061cf525cce59fc0f5e7
Boris Djuric <boris_djuric@hotmail.com>
2024-01-16 21:49:53 +0100

    Add code and functions make some tweaks and add comments

--- src/App.js
+++ src/App.js
@@ -1,3 +1,74 @@
+import { useState } from 'react';
+
 export default function App() {
-  return <div>Hello</div>;
+  // define useState for text boxes
+  const [topText, setTopText] = useState('');
+  const [bottomText, setBottomText] = useState('');
+  const [memeTemplate, setMemeTemplate] = useState('doge');
+  const [memeUrl, setMemeUrl] = useState('');
+
+  // Function to generate meme URL based on current state values
+  const generateMeme = () => {
+    const apiUrl = `https://memegen.link/${memeTemplate}/${topText}/${bottomText}.jpg`;
+    setMemeUrl(apiUrl);
+  };
+
+  // Event handler for changing meme template
+  const handleTemplateChange = (event) => {
+    setMemeTemplate(event.target.value);
+    generateMeme();
+  };
+
+  // Event handler for downloading the generated meme
+  const handleDownload = () => {
+    // Create a download link and trigger the download
+    const link = document.createElement('a');
+    link.href = memeUrl;
+    link.download = 'meme.jpg';
+    link.click();
+  };
+
+  return (
+    <>
+      <h1>Meme Generator</h1>
+      <div>
+        <label htmlFor="topText">Top text</label>
+        <br />
+        <input
+          id="topText"
+          value={topText}
+          onChange={(event) => setTopText(event.target.value)}
+        />
+        <br />
+        <hr />
+        <label htmlFor="bottomText">Bottom Text</label>
+        <br />
+        <input
+          id="bottomText"
+          value={bottomText}
+          onChange={(event) => setBottomText(event.target.value)}
+        />
+        <br />
+        <hr />
+        <label htmlFor="memeTemplate">Meme Template</label>
+        <br />
+        <input
+          id="memeTemplate"
+          value={memeTemplate}
+          onChange={handleTemplateChange}
+        />
+        <hr />
+        <br />
+        <br />
+        <img
+          src={`https://memegen.link/${memeTemplate}/${topText}/${bottomText}.jpg`}
+          alt="Generated Meme"
+          data-test-id="meme-image"
+        />
+      </div>
+      <div>
+        <button onClick={handleDownload}>Download</button>
+      </div>
+    </>
+  );
 }
